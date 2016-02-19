d198e6534b954d203e3c66352462786b4d3ee86a
Evan Funnell <funnell@mymacewan.ca>
2016-02-18 20:49:21 -0700

    add routes for goat

---
+++ app/Http/Controllers/GoalsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class GoalsController extends Controller
+{
+    public function goal() {
+        return view('layouts.goal');
+    }
+}
