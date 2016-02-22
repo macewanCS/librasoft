9f712ac7d0ac3c94c8c791bf02c5b2b6be0a5196
Evan Funnell <funnell@mymacewan.ca>
2016-02-22 08:57:43 -0700

    add buttons for new plan and goat

--- resources/views/layouts/obj.blade.php
+++ resources/views/layouts/obj.blade.php
@@ -4,16 +4,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
+                <form method="get" action="/objs/new">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Objectives</button>
+                    </div>
+                </form>
+
                 <div class="panel panel-default">
                     <div class="panel-heading">Objectives</div>
 
                     <div class="panel-body">
                         Your Goal's Objectives
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Add Action</button>
                 </div>
             </div>
         </div>
--- resources/views/layouts/task.blade.php
+++ resources/views/layouts/task.blade.php
@@ -4,16 +4,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
+                <form method="get" action="/tasks/new">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Task</button>
+                    </div>
+                </form>
+
                 <div class="panel panel-default">
                     <div class="panel-heading">Tasks</div>
 
                     <div class="panel-body">
                         Your Action's Tasks
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Add Task</button>
                 </div>
             </div>
         </div>
