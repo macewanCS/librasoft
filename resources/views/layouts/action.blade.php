c7ac67c40df86c2c0802b3fc33b245ac557e5d38
Evan Funnell <funnell@mymacewan.ca>
2016-02-22 08:57:43 -0700

    add buttons for new plan and goat

--- resources/views/layouts/action.blade.php
+++ resources/views/layouts/action.blade.php
@@ -4,16 +4,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
+                <form method="get" action="/actions/new">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Actions</button>
+                    </div>
+                </form>
+
                 <div class="panel panel-default">
                     <div class="panel-heading">Actions</div>
 
                     <div class="panel-body">
                         Your Objective's Actions
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Add Action</button>
                 </div>
             </div>
         </div>
