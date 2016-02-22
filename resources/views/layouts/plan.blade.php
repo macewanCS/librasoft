b654d91e05c08a36e70325ce83d551dedc0ecd7b
Evan Funnell <funnell@mymacewan.ca>
2016-02-22 08:57:43 -0700

    add buttons for new plan and goat

--- resources/views/layouts/plan.blade.php
+++ resources/views/layouts/plan.blade.php
@@ -4,16 +4,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
+                <form method="get" action="/plans/new">
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Create New Business Plan</button>
+                    </div>
+                </form>
+
                 <div class="panel panel-default">
                     <div class="panel-heading">Business Plans</div>
 
                     <div class="panel-body">
                         Your Current Business Plans
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Create New Business Plan</button>
                 </div>
             </div>
         </div>
