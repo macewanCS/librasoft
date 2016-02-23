0446e03a264af9e3dcc2290019f2803627ccb4fc
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-02-22 20:03:39 -0700

    merge

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -66,6 +66,7 @@ class AuthController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'department' => $data['department'],
             'password' => bcrypt($data['password']),
         ]);
     }
