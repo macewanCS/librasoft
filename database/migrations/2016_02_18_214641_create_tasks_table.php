226fcea45dccc82bd02d75b81eb48ce16845dca1
jonesj38 <jonesj38@mymacewan.ca>
2016-02-23 15:50:20 -0700

    fixed parsing error

--- database/migrations/2016_02_18_214641_create_tasks_table.php
+++ database/migrations/2016_02_18_214641_create_tasks_table.php
@@ -15,14 +15,11 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->text('body');
-<<<<<<< 719956cd32de488af82bd123e7a671b583e73aea
             $table->date('date');
             $table->text('lead');
             $table->text('collaborators');
             $table->text('status');
             $table->text('success');
-=======
->>>>>>> Add Migrations for goat
             $table->timestamps();
         });
     }
