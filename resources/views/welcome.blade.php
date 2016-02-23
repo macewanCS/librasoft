12382938052b786216932eeddae02b86548ee2f9
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-02-22 20:12:49 -0700

    add a new container to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,23 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Recent Updates</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    Review public computing needs and develop strategies to meet those needs. Has been completed
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">My Work is done here</div>
+
+                <div class="panel-body">
+                    Hello
                 </div>
             </div>
         </div>
