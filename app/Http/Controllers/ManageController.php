36cf695409f9117b7a9791394ccf37a5f9becbcc
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-02-23 11:00:15 -0700

    add manage controller

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -9,8 +9,12 @@ use App\Http\Controllers\Controller;
 
 class ManageController extends Controller
 {
+<<<<<<< f738b37e8b28e3e637a1af51d33d7dcf9cb7ae12
     public function manage()
     {
         return view('manage');
     }
+=======
+    public function
+>>>>>>> add manage controller
 }
