cfbffd95984aa60f25239f2628d0c96eeeb576b0
Evan Funnell <funnell@mymacewan.ca>
2016-02-23 15:27:52 -0700

    Add seeders to databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,11 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call('Tasks_Table_Seeder');
-        $this->call('Objectives_Table_Seeder');
-        $this->call('Actions_Table_Seeder');
-        $this->call('Business_Plans_Table_Seeder');
-        $this->call('Goals_Table_Seeder');
-        $this->call('Users_Table_Seeder');
+        $this->call(Business_Plans_Table_Seeder::class);
+        $this->call(Goals_Table_Seeder::class);
+        $this->call(Objectives_Table_Seeder::class);
+        $this->call(Actions_Table_Seeder::class);
+        $this->call(Tasks_Table_Seeder::class);
+        $this->call(Users_Table_Seeder::class);
     }
 }
