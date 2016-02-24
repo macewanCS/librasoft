5e3911501e209917f362d829610b67a90817e4ce
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-02-24 09:26:57 -0700

    fixing three way merge

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -9,12 +9,18 @@ use App\Http\Controllers\Controller;
 
 class ManageController extends Controller
 {
+<<<<<<< 36cf695409f9117b7a9791394ccf37a5f9becbcc
 <<<<<<< f738b37e8b28e3e637a1af51d33d7dcf9cb7ae12
+=======
+>>>>>>> changing layout of manage blade
     public function manage()
     {
         return view('manage');
     }
+<<<<<<< 36cf695409f9117b7a9791394ccf37a5f9becbcc
 =======
     public function
 >>>>>>> add manage controller
+=======
+>>>>>>> changing layout of manage blade
 }
--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -13,6 +13,7 @@
                     </div>
                 </div>
 
+<<<<<<< 36cf695409f9117b7a9791394ccf37a5f9becbcc
 <<<<<<< c78d37b8c1f0f1e072515996f56e316be8e0c3c1
                 <div class="form-group" >
                     <button  type="submit" class="btn btn-primary">Create New Business Plan</button>
@@ -20,6 +21,10 @@
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary">Create New Business Plan</button>
 >>>>>>> add manage view and controller
+=======
+                <div class="form-group" >
+                    <button  type="submit" class="btn btn-primary">Create New Business Plan</button>
+>>>>>>> changing layout of manage blade
                 </div>
             </div>
         </div>
