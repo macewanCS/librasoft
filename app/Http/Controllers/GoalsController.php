dcc32f9d772d023f6be3faa83a5e8362cbd458e7
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-02-24 22:55:34 -0700

    update changes from rebase

--- app/Http/Controllers/GoalsController.php
+++ app/Http/Controllers/GoalsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Objective;
 use Illuminate\Http\Request;
 
 use App\Goal;
--- resources/views/Goal/index.blade.php
+++ resources/views/Goal/index.blade.php
@@ -18,10 +18,12 @@
                 <div id="collapse{{$goal->id}}goal" class="panel-collapse collapse in">
                     <div class="panel-body">
                         <!--Attempt accordin 2 , if you want to see the first accordion cut this part out tell end-->
+
                         <body>
                         <div class="bs-example" style="padding-left: 40px; padding-right: 40px">
                             <div class="panel-group" id="accordion">
-                                @foreach ($goal->objectives as $objective)
+                                @foreach ($objectives as $objective)
+                                    {{!!!$counter++}}
                                     <div class="panel panel-default">
                                         <div class="panel-heading">
                                             <h4 class="panel-title">
