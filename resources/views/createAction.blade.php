3b8fcab44ac59c2e5d74f96d0b61efc774d8e135
Evan Funnell <funnell@mymacewan.ca>
2016-02-25 13:44:24 -0700

    Create wizard setup

---
+++ resources/views/createAction.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Plan Builder</div>
+
+                    <div class="panel-body">
+                        <form role="form" method="POST" action="{{ url('/plan/new') }}">
+                            <div class="form-group row" style="padding-left: 25px; padding-top: 15px">
+                                <label for="step4Label" class="col-sm-4" style="font-size: 14pt">
+                                    Step 4: Enter your Actions!
+                                </label>
+
+                                <input class="col-sm-3" name="body">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+@endsection
