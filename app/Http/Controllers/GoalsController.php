266e4a07a1d7ac6dd99c022f91aeef73eff57b90
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-02-24 21:44:26 -0700

    comment

--- app/Http/Controllers/GoalsController.php
+++ app/Http/Controllers/GoalsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Objective;
 use Illuminate\Http\Request;
 
 use App\Goal;
@@ -18,9 +19,10 @@ class GoalsController extends Controller
     public function index()
     {
         $goals = Goal::all();
+        $objectives = Objective::all();
         $counter = 0;
 
-        return view('Goal.index', compact('goals', 'counter'));
+        return view('Goal.index', compact('goals', 'counter', 'objectives'));
 
     }
 
--- resources/views/Goal/index.blade.php
+++ resources/views/Goal/index.blade.php
@@ -17,11 +17,12 @@
                 </div>
                 <div id="collapse{{$counter}}" class="panel-collapse collapse in">
                     <div class="panel-body">
-                        --Attempt accordin 2 , if you want to see the first accordion cut this part out tell end--
+                        {{--Attempt accordin 2 , if you want to see the first accordion cut this part out tell end--}}
                         <body>
                         <div class="bs-example" style="padding-left: 40px; padding-right: 40px">
                             <div class="panel-group" id="accordion">
-                                @foreach ($goal->objectives as $objective)
+                                @foreach ($objectives as $objective)
+                                    {{!!!$counter++}}
                                     <div class="panel panel-default">
                                         <div class="panel-heading">
                                             <h4 class="panel-title">
@@ -35,12 +36,12 @@
                                             </div>
                                         </div>
                                     </div>
-                                    {{!!!$counter++}}
+
                                 @endforeach
                             </div>
                         </div>
                         </body>
-                        --Attempt accordin 2, here here here--
+                        {{--Attempt accordin 2, here here here--}}
 
                     </div>
                 </div>
