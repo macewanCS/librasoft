3d571960edaed9928334e5b6189e449af0b8da11
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-03-02 10:28:16 -0700

    add owner to migrate and seed files

--- database/migrations/2016_02_18_214641_create_tasks_table.php
+++ database/migrations/2016_02_18_214641_create_tasks_table.php
@@ -14,6 +14,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('department');
             $table->text('body');
             $table->date('date');
             $table->text('lead');
