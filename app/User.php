7c9619df02778c7c639ae6273a58f12839bdc9ad
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-03-03 13:17:01 -0700

    merge issue

--- app/User.php
+++ app/User.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 
+
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
     use Authenticatable, CanResetPassword, HasRole;
