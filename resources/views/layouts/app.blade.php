662f428b7fc4d4f7e0b3d96202fd3344ac91e587
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-03-05 15:49:24 -0700

    Move mywork to mywork blade/controller, add dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,9 +75,10 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav" style="padding-top: 33px; font-size: 25px;">
-                    <li><a href="{{ url('/') }}">My Work</a></li>
+                    <li><a href="{{ url('/') }}">Dashboard</a></li>
                     <li><a href="{{ url('/plan') }}">Plan</a></li>
                     <li><a href="{{ url('/manage') }}">Archives</a></li>
+                    <li><a href="{{ url('/mywork') }}">My Work</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
