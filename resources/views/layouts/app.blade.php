97be0a9be04f79acafcf23ffefc075d365b6998f
Nathan Ell <elln2@mymacewan.ca>
2016-03-06 04:27:07 +0000

    Merge from upstream

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,54 +54,23 @@
     </style>
 </head>
 <body id="app-layout">
-    <div class="non-footer-wrapper">
-        <nav class="navbar navbar-default" style="height: 90px; background: white">
-            <div class="container">
-                <div class="navbar-header">
+    <nav class="navbar navbar-default" style="height: 90px; background: white">
+        <div class="container">
+            <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        <img src="{{asset("/img/DesktopLogo_190x70.png")}}" alt="EPL Logo" style="width:190px;height:70px;">
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav" style="padding-top: 33px; font-size: 25px;">
-                       @permission('view.mywork') <li><a href="{{ url('/') }}">My Work</a></li> @endpermission
-                        <li><a href="{{ url('/plan') }}">Plan</a></li>
-                        <li><a href="{{ url('/manage') }}">Archives</a></li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right" style="padding-top: 33px; font-size: 25px;">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Admin</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{asset("/img/DesktopLogo_190x70.png")}}" alt="EPL Logo" style="width:190px;height:70px;">
+                </a>
             </div>
-        </nav>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
@@ -134,13 +103,16 @@
         </div>
     </nav>
 
-        @yield('content')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $(".fancybox").fancybox();
+        });
+    </script>
 
-        <div class="push"></div>
-    </div>
+    @yield('content')
 
     <!-- Footer -->
-    <div class="well well-sm footer">
+    <div id="footer" class="well well-sm">
         <ul id="footer-list" class="list-inline">
             <li><a href="http://www.epl.ca/">Edmonton Public Library</a></li>
             <li><a href="https://epl.bibliocommons.com/info/terms">Terms of Use</a></li>
