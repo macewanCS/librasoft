aec59880c96f4f906bde356057d16b12a4b60ec5
Nathan Ell <elln2@mymacewan.ca>
2016-03-06 04:39:05 +0000

    Start getting footer to bottom of page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,62 +54,67 @@
     </style>
 </head>
 <body id="app-layout">
-    <nav class="navbar navbar-default" style="height: 90px; background: white">
-        <div class="container">
-            <div class="navbar-header">
+    <div class="non-footer-wrapper">
+        <nav class="navbar navbar-default" style="height: 90px; background: white">
+            <div class="container">
+                <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{asset("/img/DesktopLogo_190x70.png")}}" alt="EPL Logo" style="width:190px;height:70px;">
-                </a>
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="{{asset("/img/DesktopLogo_190x70.png")}}" alt="EPL Logo" style="width:190px;height:70px;">
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav" style="padding-top: 33px; font-size: 25px;">
+                        <li><a href="{{ url('/') }}">Dashboard</a></li>
+                        <li><a href="{{ url('/plan') }}">Plan</a></li>
+                        <li><a href="{{ url('/manage') }}">Archives</a></li>
+                        <li><a href="{{ url('/mywork') }}">My Work</a></li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right" style="padding-top: 33px; font-size: 25px;">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ url('/login') }}">Login</a></li>
+                            <li><a href="{{ url('/register') }}">Admin</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
             </div>
+        </nav>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav" style="padding-top: 33px; font-size: 25px;">
-                    <li><a href="{{ url('/') }}">Dashboard</a></li>
-                    <li><a href="{{ url('/plan') }}">Plan</a></li>
-                    <li><a href="{{ url('/manage') }}">Archives</a></li>
-                    <li><a href="{{ url('/mywork') }}">My Work</a></li>
-                </ul>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $(".fancybox").fancybox();
+            });
+        </script>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right" style="padding-top: 33px; font-size: 25px;">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Admin</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+        @yield('content')
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+        <div class="push"></div>
+    </div>
 
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $(".fancybox").fancybox();
-        });
-    </script>
-
-    @yield('content')
 
     <!-- Footer -->
     <div id="footer" class="well well-sm">
