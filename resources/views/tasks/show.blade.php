afb4700316f5e84779e03caa18b05af67d46a378
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-03-10 11:21:45 -0700

    Comment out save button

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -39,7 +39,7 @@
 
                 <!-- save all button-->
                 <div>
-                <button type="submit" class="btn btn-primary" style="background: #009FD7; float: right;">Save All</button>
+                 <!--<button type="submit" class="btn btn-primary" style="background: #009FD7; float: right;">Save All</button> -->
                 </div>
                 <!-- save all button end-->
 
