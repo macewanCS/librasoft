2553f1fa2170a350bb86da9eddfc42f1ac4b60be
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-03-10 14:56:58 -0700

    merge conflict

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,22 +3,6 @@
 
 @section('content')
 
-    <div class="dashboard-row">
-        <div class="col-xs-6 col-xs-offset-3 dashboard-panel">
-            <div class="panel panel-primary">
-                <div class="panel-heading" style="background: #009FD7">Announcements</div>
-
-                <div class="panel-body" style="height: 200px">
-                    <ul>
-                        <li> The EPL is having Bill gates as a guest speaker sometime this year.</li>
-                        <li> EPL Day celebrations at all branches on March 13, 2016</li>
-                        <li> The EPL is having Reza Aslan to speak on confronting islamaphobia on May 18, 2016</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="dashboard-row">
         <div class="col-xs-2 dashboard-panel">
             <div class="panel panel-primary">
                 <div class="panel-heading" style="background: #009FD7">Recently update</div>
