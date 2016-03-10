d126e13d85dc5573fc43c36a1b1055835d54dba8
Nathan Ell <elln2@mymacewan.ca>
2016-03-10 11:38:02 -0700

    fix register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,8 +88,9 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ url('/login') }}">Login</a></li>
+                            <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
-                            @role('admin')<li><a href="{{ url('/register') }}">Register</a></li>@endrole
+                            <li><a href="{{ url('/register') }}">Register</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
