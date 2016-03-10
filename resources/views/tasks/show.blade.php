b18552860577dbb6ddd6d418ddbd48430a11fb80
Kenneth Carmichael <carmichaelk3@mymacewan.ca>
2016-03-09 20:58:34 -0700

    Fix merge issue

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -85,9 +85,9 @@
                                 </div>
                             </form>
                         </div>
-
                     </div>
-
+                        <button type="submit" class="btn btn-primary" style="float: right" id="note">Add Note</button>
+                    </form>
                     <!-- Notes end -->
                 </div>
             </div>
