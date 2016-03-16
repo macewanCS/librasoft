4f2a2c3546eff9974f2d1c80ad3d22d5e245dcda
Nathan Ell <elln2@mymacewan.ca>
2016-03-16 10:33:53 -0600

    Create top level div and add new plan button

--- resources/views/plan.blade.php
+++ resources/views/plan.blade.php
@@ -11,122 +11,133 @@
 </div>-->
 
 <!-- Goal Group -->
+<div class="panel panel-default">
+    <div class="panel-heading" style="height: 50px;">
+        <div style="float: left; position: relative; top: 50%; transform: translateY(-50%);">
+            <p class="panel-title">Business Plan</p>
+        </div>
+        <div style="float: right;">
+            <a role="button" class="btn btn-primary" style="background: #009FD7;" href="plan/new">New Business Plan</a>
+        </div>
+    </div>
+    <div class="panel-body">
+        <div class="bs-example" style="padding-left: 40px; padding-right: 40px">
+            <div class="panel-group">
+                @foreach($plan->goals as $goal)
 
-<div class="bs-example" style="padding-left: 40px; padding-right: 40px">
-    <div class="panel-group">
-        @foreach($plan->goals as $goal)
+                    <div class="panel panel-primary">
+                        <div  onClick="toggleChevron(this)" class="panel-heading" data-toggle="collapse" href="#collapsegoal{{ $goal->id }}" style="background: #009FD7; cursor: pointer;">
+                            <h4 class="panel-title" >
+                                <a  data-toggle="collapse" href="#collapsegoal{{ $goal->id }}"><i class="glyphicon glyphicon-chevron-down"></i> {{ $goal->body }} </a>
+                            </h4>
+                        </div>
 
-            <div class="panel panel-primary">
-                <div  onClick="toggleChevron(this)" class="panel-heading" data-toggle="collapse" href="#collapsegoal{{ $goal->id }}" style="background: #009FD7; cursor: pointer;">
-                    <h4 class="panel-title" >
-                        <a  data-toggle="collapse" href="#collapsegoal{{ $goal->id }}"><i class="glyphicon glyphicon-chevron-down"></i> {{ $goal->body }} </a>
-                    </h4>
-                </div>
+                        <!-- Objective Group-->
+                        <div id="collapsegoal{{ $goal->id }}" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                <div class="bs-example2">
+                                    <div class="panel-group">
+                                        <div class="panel panel-default">
 
-                <!-- Objective Group-->
-                <div id="collapsegoal{{ $goal->id }}" class="panel-collapse collapse in">
-                    <div class="panel-body">
-                        <div class="bs-example2">
-                            <div class="panel-group">
-                                <div class="panel panel-default">
+                                            @foreach($goal->objectives as $objective)
 
-                                    @foreach($goal->objectives as $objective)
+                                                <div onClick="toggleChevron(this)" class="panel-heading" data-toggle="collapse" href="#collapseobjective{{ $objective->id }}" style="cursor: pointer;">
+                                                    <h4 class="panel-title">
+                                                        <a data-toggle="collapse" href="#collapseobjective{{ $objective->id }}"><i class="glyphicon glyphicon-chevron-right"></i> {{ $objective->body }}</a>
+                                                    </h4>
+                                                </div>
 
-                                        <div onClick="toggleChevron(this)" class="panel-heading" data-toggle="collapse" href="#collapseobjective{{ $objective->id }}" style="cursor: pointer;">
-                                            <h4 class="panel-title">
-                                                <a data-toggle="collapse" href="#collapseobjective{{ $objective->id }}"><i class="glyphicon glyphicon-chevron-right"></i> {{ $objective->body }}</a>
-                                            </h4>
-                                        </div>
-
-                                        <div id="collapseobjective{{ $objective->id }}" class="panel-collapse collapse">
-                                            <div class="panel-body">
-                                                <!--<p>{{ $objective->body }}</p>-->
+                                                <div id="collapseobjective{{ $objective->id }}" class="panel-collapse collapse">
+                                                    <div class="panel-body">
+                                                        <!--<p>{{ $objective->body }}</p>-->
 
 
-                                                <table class="table table-condensed table-bordered action-table">
-                                                    <thead>
-                                                    <tr>
-                                                        <th class="table-id">ID</th>
-                                                        <th class="table-task">Action/task</th>
-                                                        <th class="table-due">Due</th>
-                                                        <th class="table-owner">Department/Team</th>
-                                                        <th class="table-lead">Lead</th>
-                                                        <th class="table-success">Success Measures</th>
-                                                        <th class="table-status">Status</th>
-                                                    </tr>
-                                                    </thead>
-
-                                                    <tbody>
-                                                    @foreach($objective->actions as $action)
-
-                                                        <tr>
-                                                            <td class="table-id"> {{$action->item}}</td>
-                                                            <td class="table-task">{{ $action->body }}</td>
-                                                            <th class="table-due">{{ $action->date }}</th>
-                                                            <td class="table-owner">{{ $action->owner }}</td>
-                                                            <td class="table-collaborators">{{ $action->lead }}</td>
-                                                            <td class="table-success">{{ $action->success }}</td>
-                                                            <td
-                                                            @if ($action->status == "Completed")
-                                                                class="table-status success"
-                                                            @else
-                                                                class="table-status danger"
-                                                            @endif
-                                                            >
-                                                                @if ($action->status == "Completed")
-                                                                    Completed
-                                                                @else
-                                                                    In progress
-                                                                @endif
-                                                            </td>
-                                                        </tr>
-                                                        @foreach($action->tasks as $task)
+                                                        <table class="table table-condensed table-bordered action-table">
+                                                            <thead>
                                                             <tr>
-                                                                <td></td>
-                                                                <td class="table-task">
-                                                                    <a href="tasks/{{ $task->id }}">
-                                                                        {{ $task->body }}
-                                                                    </a>
-                                                                </td>
-                                                                <td class="table-due">{{ $task->date }}</td>
-                                                                <td class="table-owner">{{ $task->owner }}</td>
-                                                                <td class="table-lead">{{ $task->lead }}</td>
-                                                                <td></td>
-                                                                <td
-                                                                    @if ($task->status == "Completed")
-                                                                        class="table-status success"
-                                                                    @else
-                                                                        class="table-status danger"
-                                                                    @endif
-                                                                >
-                                                                    @if ($task->status == "Completed")
-                                                                        Completed
-                                                                    @else
-                                                                        In progress
-                                                                    @endif
-                                                                </td>
+                                                                <th class="table-id">ID</th>
+                                                                <th class="table-task">Action/task</th>
+                                                                <th class="table-due">Due</th>
+                                                                <th class="table-owner">Department/Team</th>
+                                                                <th class="table-lead">Lead</th>
+                                                                <th class="table-success">Success Measures</th>
+                                                                <th class="table-status">Status</th>
                                                             </tr>
-                                                        @endforeach
+                                                            </thead>
 
-                                                    </tbody>
+                                                            <tbody>
+                                                            @foreach($objective->actions as $action)
+
+                                                                <tr>
+                                                                    <td class="table-id"> {{$action->item}}</td>
+                                                                    <td class="table-task">{{ $action->body }}</td>
+                                                                    <th class="table-due">{{ $action->date }}</th>
+                                                                    <td class="table-owner">{{ $action->owner }}</td>
+                                                                    <td class="table-collaborators">{{ $action->lead }}</td>
+                                                                    <td class="table-success">{{ $action->success }}</td>
+                                                                    <td
+                                                                            @if ($action->status == "Completed")
+                                                                            class="table-status success"
+                                                                            @else
+                                                                            class="table-status danger"
+                                                                            @endif
+                                                                    >
+                                                                        @if ($action->status == "Completed")
+                                                                            Completed
+                                                                        @else
+                                                                            In progress
+                                                                        @endif
+                                                                    </td>
+                                                                </tr>
+                                                                @foreach($action->tasks as $task)
+                                                                    <tr>
+                                                                        <td></td>
+                                                                        <td class="table-task">
+                                                                            <a href="tasks/{{ $task->id }}">
+                                                                                {{ $task->body }}
+                                                                            </a>
+                                                                        </td>
+                                                                        <td class="table-due">{{ $task->date }}</td>
+                                                                        <td class="table-owner">{{ $task->owner }}</td>
+                                                                        <td class="table-lead">{{ $task->lead }}</td>
+                                                                        <td></td>
+                                                                        <td
+                                                                                @if ($task->status == "Completed")
+                                                                                class="table-status success"
+                                                                                @else
+                                                                                class="table-status danger"
+                                                                                @endif
+                                                                        >
+                                                                            @if ($task->status == "Completed")
+                                                                                Completed
+                                                                            @else
+                                                                                In progress
+                                                                            @endif
+                                                                        </td>
+                                                                    </tr>
+                                                                @endforeach
+
+                                                            </tbody>
 
 
-                                                    @endforeach
-                                                </table>
-                                            </div>
+                                                            @endforeach
+                                                        </table>
+                                                    </div>
+                                                </div>
+
+                                            @endforeach
+
                                         </div>
 
-                                    @endforeach
-
+                                    </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-        @endforeach
+                @endforeach
+            </div>
+        </div>
     </div>
 </div>
 
