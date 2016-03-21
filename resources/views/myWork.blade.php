d6a167c1bc56dd2a962f99a36dd839d8a73fea6a
Nathan Ell <elln2@mymacewan.ca>
2016-03-21 10:23:47 -0600

    update table contents

--- resources/views/myWork.blade.php
+++ resources/views/myWork.blade.php
@@ -3,36 +3,106 @@
 @section('content')
     <div class="bs-example" style="padding-left: 40px; padding-right: 40px;" >
         <div class="panel panel-primary">
-            <div class="panel-heading" style="background: #009FD7">In-Progress</div>
+            <div class="panel-heading" style="background: #009FD7">In Progress</div>
 
             <div class="panel-body" style="padding: 0px">
                 <?php
-                $id =  Auth::user()->name;
-                $myTasks = DB::table('tasks')->where('lead', $id)->where('status', '')->get();
+                    $id =  Auth::user()->email;
+                    $myTasks = array();
+                    $alltasks = \App\Task::all();
+                    foreach ($alltasks as $sometask) {
+                        $leads = explode("__,__", $sometask->lead);
+                        $collaborators = explode("__,__", $sometask->collaborators);
+
+                        if (in_array($id, $leads) || in_array($id, $collaborators)) {
+                            $myTasks[] = $sometask;
+                        }
+                    }
+                    $myActions = array();
+                    $allactions = \App\Action::all();
+                    foreach ($allactions as $someaction) {
+                        $leads = explode("__,__", $someaction->lead);
+                        $collaborators = explode("__,__", $someaction->collaborators);
+
+                        if (in_array($id, $leads) || in_array($id, $collaborators)) {
+                            $myActions[] = $someaction;
+                        }
+                    }
                 ?>
                 <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Action</th>
+                            <th>Task</th>
+                            <th>Due</th>
+                            <th>Owner</th>
+                            <th>Lead</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
 
-                    <!-- Header -->
-                    <tr>
-                        <th>Task</th>
-                        <th>Due</th>
-                        <th>Owner</th>
-                        <th>Lead</th>
-                        <th>Status</th>
-                    </tr>
-                    <!-- /Header -->
+                    <tbody>
+                    @foreach($myActions as $action)
+                        <tr>
+                            <td>{{ $action->item }}</td>
+                            <td>{{ $action->body }}</td>
+                            <td>{{ $action->date }}</td>
+                            <td>{{ $action->owner }}</td>
+                            <td>
+                                <?php
+                                $leads = explode("__,__", $action->lead);
+                                foreach ($leads as $lead) {
+                                    // Check if it's a valid email address
+                                    if (filter_var($lead, FILTER_VALIDATE_EMAIL)) {
+                                        echo \App\User::where("email", $lead)->first()->name;
+                                    } else {
+                                        echo $lead;
+                                    }
 
-                    <!-- Body -->
+                                    if ($lead != $leads[count($leads)-1]) {
+                                        echo ", ";
+                                    }
+                                }
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                if ($action->status == "done") {
+                                    echo "Done";
+                                } else {
+                                    echo "In progress";
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                    @endforeach
                     @foreach($myTasks as $task)
                         <tr>
+                            <td></td>
                             <td>
-                                <a href="tasks/{{ $task->id }}">
+                                <a href="/tasks/{{ $task->id }}">
                                     {{ $task->body }}
                                 </a>
                             </td>
                             <td>{{ $task->date }}</td>
                             <td>{{ $task->owner }}</td>
-                            <td>{{ $task->lead }}</td>
+                            <td>
+                                <?php
+                                $leads = explode("__,__", $task->lead);
+                                foreach ($leads as $lead) {
+                                    // Check if it's a valid email address
+                                    if (filter_var($lead, FILTER_VALIDATE_EMAIL)) {
+                                        echo \App\User::where("email", $lead)->first()->name;
+                                    } else {
+                                        echo $lead;
+                                    }
+
+                                    if ($lead != $leads[count($leads)-1]) {
+                                        echo ", ";
+                                    }
+                                }
+                                ?>
+                            </td>
                             <td>
                                 <?php
                                 if ($task->status == "done") {
@@ -43,61 +113,8 @@
                                 ?>
                             </td>
                         </tr>
-                        @endforeach
-                                <!-- /Body -->
-
-                </table>
-            </div>
-
-        </div>
-    </div>
-
-    <div class="bs-example" style="padding-left: 40px; padding-right: 40px">
-        <div class="panel panel-primary">
-
-            <div class="panel-heading" style="background: #009FD7">Completed Tasks</div>
-
-            <div class="panel-body" style="padding: 0px">
-                <?php
-                $id =  Auth::user()->name;
-                $myTasks = DB::table('tasks')->where('lead', $id)->where('status', 'done')->get();
-                ?>
-                <table class="table table-striped table-bordered table-hover">
-
-                    <!-- Header -->
-                    <tr>
-                        <th>Task</th>
-                        <th>Due</th>
-                        <th>Owner</th>
-                        <th>Lead</th>
-                        <th>Status</th>
-                    </tr>
-                    <!-- /Header -->
-
-                    <!-- Body -->
-                    @foreach($myTasks as $task)
-                    <tr>
-                        <td>
-                            <a class="fancybox" href="tasks/{{ $task->id }}">
-                                {{ $task->body }}
-                            </a>
-                        </td>
-                        <td>{{ $task->date }}</td>
-                        <td>{{ $task->owner }}</td>
-                        <td>{{ $task->lead }}</td>
-                        <td>
-                            <?php
-                            if ($task->status == "done") {
-                                echo "Done";
-                            } else {
-                                echo "In progress";
-                            }
-                            ?>
-                        </td>
-                    </tr>
                     @endforeach
-                    <!-- /Body -->
-
+                    </tbody>
                 </table>
             </div>
 
