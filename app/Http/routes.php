56945a86d2ec56f3dd4fb4701d4ef773371bb428
kenneth carmichael <carmichael@kenneths-MacBook-Pro.local>
2016-03-22 21:50:47 -0600

    Add populating to recently updated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome')->with('plan', \App\Plan::first());
+    //$act = DB::table('actions')->orderby('updated_at', 'desc')->get();
+
+    return view('welcome')->with('plan', \App\Plan::first())->with('act',DB::table('actions')->orderby('updated_at', 'desc')->get());
 });
 
 
@@ -44,7 +46,8 @@ Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::get('/', function () {
-        return view('welcome')->with('plan', \App\Plan::first());
+        //$act = DB::table('actions')->orderby('updated_at', 'desc')->get();
+        return view('welcome')->with('plan', \App\Plan::first())->with('act',DB::table('actions')->orderby('updated_at', 'desc')->get());
     });
 
     Route::get('plan', 'PlanController@plan');
