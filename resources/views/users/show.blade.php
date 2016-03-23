45c40c5f66881a9b547783ab85c7c7a5fabb9a57
Nathan Ell <elln2@mymacewan.ca>
2016-03-23 11:22:39 -0600

    show a specific searched model

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="panel panel-primary">
+        <div class="panel-heading"><h4 class="panel-title">{{ $user->name }}</h4></div>
+    </div>
+
+@stop
