c9ab9ae31af5b1b415935418be9f85e9bfbb9c47
Evan Funnell <funnell@mymacewan.ca>
2016-03-24 14:04:15 -0600

    Add Goal and Objective textareas

--- resources/views/createPlan.blade.php
+++ resources/views/createPlan.blade.php
@@ -28,13 +28,55 @@
                     <div class="panel-body">
                         Complete the following steps to get your new Business Plan up and running quickly!
                         <form role="form" method="POST" action="{{ url('/plan/new') }}">
-                            <div class="form-group row" style="padding-left: 25px; padding-top: 30px;">
-                                <label for="step1Label" class="col-sm-4" style="font-size: 14pt;">
+                            <div class="form-group" style="padding-left: 25px; padding-top: 30px;">
+                                <label for="step1Label" style="font-size: 14pt;">
                                     Step 1: Choose a plan year range:
                                 </label>
                                 <select name="startdate" id="startpicker"></select>
                                 <select name="enddate" id="endpicker"></select>
                             </div>
+
+                            <div class="form-group" style="padding-left: 25px;">
+                                <label for="step2Label" style="font-size: 14pt;">Step 2:</label>
+                                <div>
+                                    <label for="goal1Label" class="col-sm-2" style="font-size: 14pt;">Goal 1 name:</label>
+                                    <textarea name="goal1" rows="1" required style="resize:none"></textarea>
+                                </div>
+                                <div>
+                                    <label for="goal2Label" class="col-sm-2" style="font-size: 14pt;">Goal 2 name:</label>
+                                    <textarea name="goal2" rows="1" required style="resize:none"></textarea>
+                                </div>
+                                <div>
+                                    <label for="goal3Label" class="col-sm-2" style="font-size: 14pt;">Goal 3 name:</label>
+                                    <textarea name="goal3" rows="1" required style="resize:none"></textarea>
+                                </div>
+                                <div>
+                                    <label for="goal4Label" class="col-sm-2" style="font-size: 14pt;">Goal 4 name:</label>
+                                    <textarea name="goal4" rows="1" required style="resize:none"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="form-group" style="padding-left: 25px">
+                                <label for="step3Label" style="font-size: 14pt;">Step 3:</label>
+                                <div>
+                                    <label for="obj1Label" class="col-sm-3" style="font-size: 14pt;">Objective 1 name:</label>
+                                    <textarea name="obj1" rows="1" required style="resize:none"></textarea>
+                                </div>
+                                <div>
+                                    <label for="obj2Label" class="col-sm-3" style="font-size: 14pt;">Objective 2 name:</label>
+                                    <textarea name="obj2" rows="1" required style="resize:none"></textarea>
+                                </div>
+                                <div>
+                                    <label for="obj3Label" class="col-sm-3" style="font-size: 14pt;">Objective 3 name:</label>
+                                    <textarea name="obj3" rows="1" required style="resize:none"></textarea>
+                                </div>
+                                <div>
+                                    <label for="obj4Label" class="col-sm-3" style="font-size: 14pt;">Objective 4 name:</label>
+                                    <textarea name="obj4" rows="1" required style="resize:none"></textarea>
+                                </div>
+                            </div>
+
+
                             <input type="hidden" name="_token" value="{{csrf_token()}}">
                             <div>
                                 <button class="btn btn-primary col-sm-1" type="submit" style="position: relative; left: 30px; top: 5px">Submit</button>
