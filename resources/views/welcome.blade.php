ba90f29a4b7e60edecdd62ae35e69d123af646b7
kenneth carmichael <carmichael@kenneths-MacBook-Pro.local>
2016-03-25 15:21:24 -0600

    Refactor due dates finished, update seeders / migratations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@
                                                 }
                                             }
                                             ?>
-                                            , <bold>Department:</bold> {{$task->owner}}, <bold>Updated:</bold> {{$action->updated_at}}
+                                            , <bold>Department:</bold> {{$action->owner}}, <bold>Updated:</bold> {{$action->updated_at}}
                                             </p>
                                         </li>
                                         @endif
@@ -189,67 +189,23 @@
         </div>
         <div class="col-xs-2 dashboard-panel">
             <div class="panel panel-primary">
-                <div class="panel-heading" style="background: #009FD7">Due Dates
+                <div class="panel-heading" style="background: #009FD7">Upcomming Due Dates
                     <a onclick="popupDates()" role="button" class="btn btn-primary background-blue-float">More</a>
                 </div>
 
                 <div class="panel-body overflow-y-height">
                     <ul class="list-group">
-                        <?php
-                            $today = Carbon\Carbon::now();
-                            $monthLess = $today->subMonth(1);
-                            $monthGreater = $today->addMonth(3);
-                        ?>
+                        <li class="list-group-item">Actions:</li>
                         @foreach($act as $action)
+                            <?php
+                                $today = Carbon\Carbon::now();
+                            ?>
                             @if($action->status != "Completed")
                                 @if($action->date > $today)
-                                    @if($action->date < $monthGreater)
-                                        <li class="list-group-item">{{$action->item}}---{{$action->body}}---{{$action->status}}--{{$action->date}}</li>
-                                        <li class="list-group-item">{{$monthLess}}----{{$monthGreater}}</li>
-                                    @endif
-                                @endif
-                            @endif
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    </div>
-
-    <!-- Popup divs-->
-    <!--Due date popup -->
-    <div class="hide fade">
-        <div id="date"  title="Dute Dates" class="panel panel-primary">
-            <div class="panel-body overflow-y: scroll" >
-                <table class="table table-condensed table-bordered action-table" style="font-size: 60%">
-                    <thead>
-                    <tr>
-                        <th class="ui-table-id">ID</th>
-                        <th class="ui-table-body">Action/Task</th>
-                        <th class="ui-table-owner">Department/ Team</th>
-                        <th class="ui-table-lead">Lead</th>
-                        <th class="ui-table-due">Date</th>
-                        <th class="ui-table-status">Status</th>
-                    </tr>
-                    </thead>
-                    @foreach($plan->goals as $goal)
-                        @foreach($goal->objectives as $objective)
-                            @foreach($objective->actions as $action)
-                                <?php
-                                $today = Carbon\Carbon::now();
-                                ?>
-                                @if ($action->date == $today)
-
-                                @elseif($action->date < $today)
-                                    @if($action->status != 'Completed')
-                                        <tbody>
-                                        <tr>
-                                            <td class="ui-table-id">{{$action->item}}</td>
-                                            <td class="ui-table-body">{{$action->body}}</td>
-                                            <td class="ui-table-owner">{{$action->owner}}</td>
-                                            <td class="ui-table-lead">
+                                    @if($action->date < $today->addMonth(3))
+                                        <li class="list-group-item"><a href="">{{$action->body}}</a>
+                                             <p class="ul-fontSize">
+                                                - <bold>Lead: </bold>
                                                 <?php
                                                     $leads = explode("__,__", $action->lead);
                                                     foreach ($leads as $lead) {
@@ -265,68 +221,129 @@
                                                         }
                                                     }
                                                 ?>
-                                            </td>
-                                            <td class="ui-table-due">{{$action->date}}</td>
-                                            <td style="font-size: 13px; min-width: 100px; max-width: 100px;"
-                                                @if($action->status == 'Ongoing')
-                                                class="success"
-                                                @elseif($action->status == 'In progress')
-                                                class="danger"
-                                                    @endif
-
-                                            >{{$action->status}}</td>
-                                        </tr>
-
-                                        @foreach($action->tasks as $task)
-                                            @if($task->date == $today)
-                                            @elseif($task->date < $today)
-                                                @if($task->status != 'Completed')
-                                                    <tr>
-                                                        <td class="ui-table-id">{{$task->item}}</td>
-                                                        <td class="ui-table-body">
-                                                            <a href="tasks/{{ $task->id }}" style="color: #3D80BA">
-                                                                {{ $task->body }}
-                                                            </a></td>
-                                                        <td class="ui-table-owner">{{$task->owner}}</td>
-                                                        <td class="ui-table-lead">
-                                                            <?php
-                                                                $leads = explode("__,__", $task->lead);
-                                                                foreach ($leads as $lead) {
-                                                                    // Check if it's a valid email address
-                                                                    if (filter_var($lead, FILTER_VALIDATE_EMAIL)) {
-                                                                        echo \App\User::where("email", $lead)->first()->name;
-                                                                    } else {
-                                                                        echo $lead;
-                                                                    }
-
-                                                                    if ($lead != $leads[count($leads)-1]) {
-                                                                        echo ", ";
-                                                                    }
-                                                                }
-                                                            ?>
-                                                        </td>
-                                                        <td class="ui-table-due">{{$task->date}}</td>
-                                                        <td style="font-size: 13px; min-width: 100px; max-width: 100px;"
-                                                            @if($task->status == 'Ongoing')
-                                                            class="success"
-                                                            @elseif($task->status == 'In progress')
-                                                            class="danger"
-                                                                @endif
-                                                        >{{$task->status}}</td>
-                                                    </tr>
-                                                @endif
-                                            @elseif($task->date > $today)
-                                            @endif
-                                        @endforeach
-                                        </tbody>
+                                                , <bold>Department: </bold> {{$action->owner}},  <bold>Due Date: </bold> {{$action->date}}
+                                             </p>
+                                        </li>
                                     @endif
-                                @elseif ($action->date > $today) 
+                                @endif
+                            @endif
+                        @endforeach
+                        <li class="list-group-item">Tasks:</li>
+                            @foreach($tasks as $task)
+                                <?php
+                                    $today = Carbon\Carbon::now();
+                                ?>
+                                @if($task->status != "Completed")
+                                    @if($task->date > $today)
+                                        @if($task->date < $today->addMonth(3))
+                                            <li class="list-group-item">
+                                                <a href="tasks/{{ $task->id }}">{{$task->body}}</a>
+                                                <p class="ul-fontSize">
+                                                    - <bold>Lead: </bold>
+                                                    <?php
+                                                        $leads = explode("__,__", $task->lead);
+                                                        foreach ($leads as $lead) {
+                                                            // Check if it's a valid email address
+                                                            if (filter_var($lead, FILTER_VALIDATE_EMAIL)) {
+                                                                echo \App\User::where("email", $lead)->first()->name;
+                                                            } else {
+                                                                echo $lead;
+                                                            }
 
+                                                            if ($lead != $leads[count($leads)-1]) {
+                                                                echo ", ";
+                                                            }
+                                                        }
+                                                    ?>
+                                                    , <bold>Department: </bold> {{$task->owner}}, <bold>Due Date: </bold> {{$task->date}}
+                                                </p>
+                                            </li>
+                                        @endif
+                                    @endif
                                 @endif
                             @endforeach
-                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+    <!-- Popup divs-->
+    <!--Due date popup -->
+    <div class="hide fade">
+        <div id="date"  title="Upcomming Due Dates" class="panel panel-primary">
+            <div class="panel-body overflow-y: scroll" >
+                <ul class="list-group">
+                    <li class="list-group-item">Actions:</li>
+                    @foreach($act as $action)
+                        <?php
+                        $today = Carbon\Carbon::now();
+                        ?>
+                        @if($action->status != "Completed")
+                            @if($action->date > $today)
+                                @if($action->date < $today->addMonth(3))
+                                    <li class="list-group-item"><a href="" style="color: #3D80BA">{{$action->body}}</a>
+                                        <p class="ul-fontSize">
+                                            - <bold>Lead: </bold>
+                                            <?php
+                                            $leads = explode("__,__", $action->lead);
+                                            foreach ($leads as $lead) {
+                                                // Check if it's a valid email address
+                                                if (filter_var($lead, FILTER_VALIDATE_EMAIL)) {
+                                                    echo \App\User::where("email", $lead)->first()->name;
+                                                } else {
+                                                    echo $lead;
+                                                }
+
+                                                if ($lead != $leads[count($leads)-1]) {
+                                                    echo ", ";
+                                                }
+                                            }
+                                            ?>
+                                            , <bold>Department: </bold> {{$action->owner}},  <bold>Due Date: </bold> {{$action->date}}
+                                        </p>
+                                    </li>
+                                @endif
+                            @endif
+                        @endif
                     @endforeach
-                </table>
+                    <li class="list-group-item">Tasks:</li>
+                    @foreach($tasks as $task)
+                        <?php
+                        $today = Carbon\Carbon::now();
+                        ?>
+                        @if($task->status != "Completed")
+                            @if($task->date > $today)
+                                @if($task->date < $today->addMonth(3))
+                                    <li class="list-group-item">
+                                        <a href="tasks/{{ $task->id }}" style="color: #3D80BA">{{$task->body}}</a>
+                                        <p class="ul-fontSize">
+                                            - <bold>Lead: </bold>
+                                            <?php
+                                            $leads = explode("__,__", $task->lead);
+                                            foreach ($leads as $lead) {
+                                                // Check if it's a valid email address
+                                                if (filter_var($lead, FILTER_VALIDATE_EMAIL)) {
+                                                    echo \App\User::where("email", $lead)->first()->name;
+                                                } else {
+                                                    echo $lead;
+                                                }
+
+                                                if ($lead != $leads[count($leads)-1]) {
+                                                    echo ", ";
+                                                }
+                                            }
+                                            ?>
+                                            , <bold>Department: </bold> {{$task->owner}}, <bold>Due Date: </bold> {{$task->date}}
+                                        </p>
+                                    </li>
+                                @endif
+                            @endif
+                        @endif
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
