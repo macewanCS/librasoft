f3415309dd44dbe89f3e304f4eec59eacc9f3bfe
Nathan Ell <elln2@mymacewan.ca>
2016-03-27 13:28:46 -0600

    migrate styles to css when possible

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -4,8 +4,8 @@
 
     <div class="bs-example">
         <div class="panel panel-primary">
-            <div class="panel-heading"><h4 class="panel-title">{{ $team->name }}</h4></div>
-            <div class="panel-body">
+            <div class="panel-heading" style="background: #009FD7;"><h4 class="panel-title">{{ $team->name }}</h4></div>
+            <div class="panel-body small-panel-body">
                 <table class="table table-bordered table-striped table-hover">
                     <th>Actions</th>
                     <?php use App\Action; $actions = Action::where('owner', 'like', '%' . $team->name . '%')->get(); ?>
@@ -14,20 +14,24 @@
                             <td><a href="/actions/show/{{ $action->id }}">{{ $action->body }}</a></td>
                         </tr>
                     @endforeach
-                    <th>Tasks</th>
                     <?php use App\Task; $tasks = Task::where('owner', 'like', '%' . $team->name . '%')->get(); ?>
-                    @foreach($tasks as $task)
-                        <tr>
-                            <td><a href="/actions/show/{{ $task->id }}">{{ $task->body }}</a></td>
-                        </tr>
-                    @endforeach
-                    <th>Users</th>
+                    @if(count($tasks) > 0)
+                        <th>Tasks</th>
+                        @foreach($tasks as $task)
+                            <tr>
+                                <td><a href="/actions/show/{{ $task->id }}">{{ $task->body }}</a></td>
+                            </tr>
+                        @endforeach
+                    @endif
                     <?php use App\User; $users = User::where('department', 'like', '%' . $team->name . '%')->get(); ?>
-                    @foreach($users as $user)
-                        <tr>
-                            <td><a href="/actions/show/{{ $user->id }}">{{ $user->name }}</a></td>
-                        </tr>
-                    @endforeach
+                    @if(count($users) > 0)
+                        <th>Users</th>
+                        @foreach($users as $user)
+                            <tr>
+                                <td><a href="/actions/show/{{ $user->id }}">{{ $user->name }}</a></td>
+                            </tr>
+                        @endforeach
+                    @endif
                 </table>
             </div>
         </div>
