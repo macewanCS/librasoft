5e112cf644f877deacd32fb20ddc599fd54f9fb7
Nathan Ell <elln2@mymacewan.ca>
2016-04-02 05:49:03 +0000

    update for new plan wizard

--- app/Objective.php
+++ app/Objective.php
@@ -11,7 +11,7 @@ class Objective extends Model
      *
      * @var array
      */
-    protected $fillable = ['body'];
+    protected $fillable = ['body', 'goal_id'];
 
     public function goal()
     {
