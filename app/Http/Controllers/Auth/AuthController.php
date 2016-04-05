cf118a759c6382a0fae31eecc544b34f6899f08c
kenneth carmichael <carmichaelk3@mymacewan.ca>
2016-04-04 21:50:22 -0600

    fixed login redirecting home route that we dont have

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    //protected $redirectTo = '/';
+    protected $redirectTo = '/';
 
     /**
      * Create a new authentication controller instance.
