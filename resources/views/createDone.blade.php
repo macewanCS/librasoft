700ec367d4faea3bfe2ece9dc69622d15ef7165f
Evan Funnell <funnelle@mymacewan.ca>
2016-04-06 10:10:36 -0600

    remove form from createDone

--- resources/views/createDone.blade.php
+++ resources/views/createDone.blade.php
@@ -9,16 +9,14 @@
                     <div class="panel-heading">Plan Builder</div>
 
                     <div class="panel-body">
-                        <form role="form" method="POST" action="{{ url('/plan/new/goal') }}">
-                            <div class="form-group row" style="padding-left: 25px; padding-top: 15px">
-                                <label for="step2Label" class="col-sm-8" style="font-size: 14pt">
-                                    Congratulations! Your new business plan is finished!
-                                </label>
-                            </div>
-                            <div>
-                                <button class="col-sm-1" type="submit" style="position: relative; left: 30px; top: 10px">Next</button>
-                            </div>
-                        </form>
+                        <div style="padding-left: 25px; padding-top: 15px">
+                            <label for="step2Label" class="col-sm-8" style="font-size: 14pt">
+                                Congratulations! Your new business plan is finished!
+                            </label>
+                        </div>
+                        <div>
+                            <button class="col-sm-1" type="button" style="position: relative; left: 30px; top: 10px">Next</button>
+                        </div>
                     </div>
                 </div>
             </div>
