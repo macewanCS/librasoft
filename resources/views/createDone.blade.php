366f28a1f8bf64a2bedf14fd4b10280bdd56c8ce
Evan Funnell <funnelle@mymacewan.ca>
2016-04-06 12:42:06 -0600

    Proper create plan used, finished

--- resources/views/createDone.blade.php
+++ resources/views/createDone.blade.php
@@ -5,18 +5,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Plan Builder</div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading" style="background: #009FD7;">Plan Builder</div>
 
                     <div class="panel-body">
                         <div class="pb-step">
                             <label for="step2Label" class="pb-label">
                                 Congratulations! Your new business plan is finished!
                             </label>
+                            <form method="get" action="/plan">
+                                <button class="btn btn-primary pb-btn" type="submit">Next</button>
+                            </form>
                         </div>
-                        <form method="get" action="/plan">
-                            <button class="pb-btn" type="submit">Next</button>
-                        </form>
                     </div>
                 </div>
             </div>
